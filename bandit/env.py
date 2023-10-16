7b370c12112462ef1df0b4757056959c9a28a6ef
Jangsea Park <jangsea.park@gmail.com>
2023-10-16 15:39:33 +0900

    Clarify adding sigma

--- bandit/env.py
+++ bandit/env.py
@@ -7,20 +7,20 @@ class BanditEnv:
         self,
         n_arms: int,
         pay_probs: list[float],
-        rewards: list[float] | list[tuple[float, float]],
+        reward_params: list[float] | list[tuple[float, float]],
         seed: int | None = None,
     ):
-        self._check_validity_number_of_arms(n_arms, pay_probs, rewards)
+        self._check_validity_number_of_arms(n_arms, pay_probs, reward_params)
         self._check_validity_pay_probs(pay_probs)
 
         self._n_arms = n_arms
-        self._pay_dist = pay_probs
-        self._rewards = rewards
+        self._pay_probs = pay_probs
+        self._reward_params = self._add_sigma(reward_params)
         self._rng = random.Random(seed)
 
-    def _check_validity_number_of_arms(self, n_arms, pay_probs, reward_dist):
+    def _check_validity_number_of_arms(self, n_arms, pay_probs, reward_params):
         n_pay_dist = len(pay_probs)
-        n_reward_dist = len(reward_dist)
+        n_reward_dist = len(reward_params)
 
         if not (n_arms == n_pay_dist == n_reward_dist):
             raise ValueError(
@@ -32,14 +32,25 @@ class BanditEnv:
         if not all(0 <= p <= 1 for p in pay_probs):
             raise ValueError("pay_dist must be a list of floats between 0 and 1")
 
-    def step(self, action: int):
-        reward_params = self._rewards[action] if self._does_pay(action) else 0
+    def _add_sigma(
+        self, reward_params: list[float] | list[tuple[float, float]]
+    ) -> list[tuple[float, float]]:
+        result = []
+        placeholder_sigma = 0.0
 
-        if not isinstance(reward_params, Iterable):
-            reward_params = (reward_params, 0)
+        for params in reward_params:
+            match params:
+                case float() | int():
+                    result.append((params, placeholder_sigma))
+                case tuple():
+                    result.append(params)
 
-        reward = self._rng.normalvariate(*reward_params)
-        return reward
+        return result
 
     def _does_pay(self, action: int) -> bool:
-        return self._rng.random() < self._pay_dist[action]
+        return self._rng.random() < self._pay_probs[action]
+
+    def step(self, action: int):
+        mu, sigma = self._reward_params[action]
+        reward = self._rng.normalvariate(mu, sigma) if self._does_pay(action) else 0
+        return reward
