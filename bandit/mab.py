de5bf65f33a78b0ef54d147527899306c2cad69f
Jangsea Park <jangsea.park@gmail.com>
2023-10-18 18:00:40 +0900

    Introduce pure exploration

--- bandit/mab.py
+++ bandit/mab.py
@@ -1,3 +1,5 @@
+import random
+
 import numpy as np
 
 from bandit.env import BanditEnv
@@ -12,6 +14,30 @@ def pure_exploitation(env: BanditEnv, n_episodes: int = 5000) -> tuple[np.ndarra
 
     for e in range(n_episodes):
         action = int(np.argmax(action_values))
+        reward = env.step(action)
+
+        n_trials[action] += 1
+        action_values[action] += (reward - action_values[action]) / n_trials[action]
+
+        actions_episodes[e] = action
+        returns_episodes[e] = reward
+        action_values_episodes[e] = action_values
+
+    return actions_episodes, returns_episodes, action_values_episodes
+
+
+def pure_exploration(
+    env: BanditEnv, n_episodes: int = 5000, seed: int | None = None
+) -> tuple[np.ndarray, ...]:
+    action_values = np.zeros(env.n_arms)
+    n_trials = np.zeros(env.n_arms)
+    action_values_episodes = np.empty((n_episodes, env.n_arms))
+    returns_episodes = np.empty(n_episodes)
+    actions_episodes = np.empty(n_episodes, dtype=int)
+    rng = np.random.default_rng(seed)
+
+    for e in range(n_episodes):
+        action = rng.integers(0, env.n_arms)
         reward = env.step(action)
 
         n_trials[action] += 1
--- bandit/tests/test_mab.py
+++ bandit/tests/test_mab.py
@@ -1,7 +1,7 @@
 from numpy.testing import assert_almost_equal, assert_array_equal
 
 from bandit.env import BanditEnv
-from bandit.mab import pure_exploitation
+from bandit.mab import pure_exploitation, pure_exploration
 
 
 def test_pure_exploitation():
@@ -18,5 +18,25 @@ def test_pure_exploitation():
             [0.33333333, 0.0],
             [0.25, 0.0],
             [0.2, 0.0],
+        ],
+    )
+
+
+def test_pure_exploration():
+    slippery_bandit_walk = BanditEnv(2, [0.2, 0.8], [1, 1], seed=1019)
+    actions, returns, values = pure_exploration(
+        slippery_bandit_walk, n_episodes=5, seed=1020
+    )
+
+    assert_array_equal(actions, [1, 1, 1, 0, 0])
+    assert_array_equal(returns, [1, 1, 1, 0, 1])
+    assert_almost_equal(
+        values,
+        [
+            [0.0, 1.0],
+            [0.0, 1.0],
+            [0.0, 1.0],
+            [0.0, 1.0],
+            [0.5, 1.0],
         ],
     )
